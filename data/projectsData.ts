228b119fcda57fbad4455a5835219d1f452070d7
Ro-ee Tal <rttal@amazon.com>
2024-10-03 18:56:44 +0200

    feat: add old projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,11 +7,27 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'Another Craigslist Bot',
+    title: 'another Craigslist bot',
     description: `I created a bot to find a bike during COVID, which was not easy because of supply-chain issues.
     The story, and link to repo, is in the post.`,
     imgSrc: '/static/images/tessa.jpg',
     href: '/blog/tessa',
+  },
+  {
+    title: 'another robot with eyes',
+    description: `We built a robot, from scratch, that used YOLOv2 for real-time object detection and navigation.`,
+    imgSrc: '/static/images/glalal.png',
+    href: 'https://github.com/roeetal/enph253',
+  },
+  {
+    title: 'neural network robustness certification',
+    description: `In this project, we implemented a learning based algorithm to improve the precision of sound zonotope certification.`,
+    href: 'https://github.com/tomginsberg/DeepZAdapt',
+  },
+  {
+    title: 'another crypto bot',
+    description: `I got COVID, so I rested in my bed for a week making an automatic crypto trading algorithm based on a statistical arbitrage strategy.`,
+    href: 'https://github.com/roeetal/traderbot',
   },
 ]
 
