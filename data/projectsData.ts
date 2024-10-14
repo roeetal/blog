b019579316c46a6fd9b6b549e65a9ec9b30d0f0b
Ro-ee Tal <rttal@amazon.com>
2024-10-14 10:54:48 -0400

    feat: projects data images

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,6 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'another crypto bot',
+    description: `I got COVID, so I rested in my bed for a week making an automatic crypto trading algorithm based on a statistical arbitrage strategy.`,
+    imgSrc: '/static/images/statistical-arbitrage.jpg',
+    href: 'https://github.com/roeetal/traderbot',
+  },
+  {
     title: 'another Craigslist bot',
     description: `I created a bot to find a bike during COVID, which was not easy because of supply-chain issues.
     The story, and link to repo, is in the post.`,
@@ -14,20 +20,16 @@ const projectsData: Project[] = [
     href: '/blog/tessa',
   },
   {
+    title: 'neural network robustness certification',
+    description: `In this project, we implemented a learning based algorithm to improve the precision of sound zonotope certification.`,
+    imgSrc: '/static/images/zonotope.png',
+    href: 'https://github.com/tomginsberg/DeepZAdapt',
+  },
+  {
     title: 'another robot with eyes',
     description: `We built a robot, from scratch, that used YOLOv2 for real-time object detection and navigation.`,
     imgSrc: '/static/images/glalal.png',
     href: 'https://github.com/roeetal/enph253',
-  },
-  {
-    title: 'neural network robustness certification',
-    description: `In this project, we implemented a learning based algorithm to improve the precision of sound zonotope certification.`,
-    href: 'https://github.com/tomginsberg/DeepZAdapt',
-  },
-  {
-    title: 'another crypto bot',
-    description: `I got COVID, so I rested in my bed for a week making an automatic crypto trading algorithm based on a statistical arbitrage strategy.`,
-    href: 'https://github.com/roeetal/traderbot',
   },
 ]
 
